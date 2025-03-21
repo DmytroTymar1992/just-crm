adc5935eb1282b6c72bee22612dfc5f24467f10d
DmytroTymar1992 <timardmitro@gmail.com>
2025-03-21 13:03:05 +0000

    Added analytics functionality with manager activity tracking and dashboard

--- sales_analytics/views.py
+++ sales_analytics/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from .models import ManagerActivity
+from main.models import Company
+from sales.models import Contact
 from django.utils import timezone
 from datetime import datetime, timedelta
 from django.db.models import Count, Q
@@ -16,51 +18,79 @@ def analytics_dashboard(request):
     date_from_str = request.GET.get('date_from')
     date_to_str = request.GET.get('date_to')
 
-    # Базовий queryset
-    activities = ManagerActivity.objects.all()
+    # Встановлюємо період за замовчуванням: сьогодні та -7 днів
+    today = timezone.now().date()
+    default_date_from = today - timedelta(days=7)
+    default_date_to = today
+
+    # Якщо дати не вказані, використовуємо період за замовчуванням
+    if not date_from_str:
+        date_from = default_date_from
+        date_from_str = date_from.strftime('%Y-%m-%d')
+    else:
+        date_from = datetime.strptime(date_from_str, '%Y-%m-%d')
+
+    if not date_to_str:
+        date_to = default_date_to
+        date_to_str = date_to.strftime('%Y-%m-%d')
+    else:
+        date_to = datetime.strptime(date_to_str, '%Y-%m-%d')
+
+    # Базовий queryset із фільтром по датах
+    activities = ManagerActivity.objects.filter(
+        date__gte=date_from,
+        date__lt=date_to + timedelta(days=1)  # Включаємо весь день date_to
+    )
 
     # Фільтр по менеджеру
     if manager_id != 'all' and manager_id:
         activities = activities.filter(manager_id=manager_id)
-
-    # Фільтр по датах
-    if date_from_str:
-        date_from = datetime.strptime(date_from_str, '%Y-%m-%d')
-        activities = activities.filter(date__gte=date_from)
-    if date_to_str:
-        date_to = datetime.strptime(date_to_str, '%Y-%m-%d') + timedelta(days=1)  # Включаємо весь день
-        activities = activities.filter(date__lt=date_to)
+        selected_manager = User.objects.get(id=manager_id)
+    else:
+        selected_manager = None
 
     # Отримання всіх менеджерів для фільтра
     managers = User.objects.all()
 
     # Дані для діаграм
-    total_companies = Company.objects.count()
-    total_contacts = Contact.objects.count()
+    # Загальна кількість створених компаній за період (усіма менеджерами)
+    total_created_companies = ManagerActivity.objects.filter(
+        activity_type='create_company',
+        date__gte=date_from,
+        date__lt=date_to + timedelta(days=1)
+    ).count()
 
-    if manager_id != 'all' and manager_id:
-        manager = User.objects.get(id=manager_id)
+    # Загальна кількість створених контактів за період (усіма менеджерами)
+    total_created_contacts = ManagerActivity.objects.filter(
+        activity_type='create_contact',
+        date__gte=date_from,
+        date__lt=date_to + timedelta(days=1)
+    ).count()
+
+    # Кількість створених компаній і контактів вибраним менеджером за період
+    if selected_manager:
         manager_created_companies = activities.filter(activity_type='create_company').count()
         manager_created_contacts = activities.filter(activity_type='create_contact').count()
     else:
-        manager = None
-        manager_created_companies = activities.filter(activity_type='create_company').aggregate(total=Count('id'))['total'] or 0
-        manager_created_contacts = activities.filter(activity_type='create_contact').aggregate(total=Count('id'))['total'] or 0
+        # Якщо вибрано "Всі", показуємо загальну кількість для всіх менеджерів
+        manager_created_companies = total_created_companies
+        manager_created_contacts = total_created_contacts
 
+    # Дзвінки за період
     incoming_calls = activities.filter(activity_type='call_in').count()
     outgoing_calls = activities.filter(activity_type='call_out').count()
 
     # Дані для логу
-    log_entries = activities.order_by('-date')[:50]  # Обмежуємо до 50 записів для відображення
+    log_entries = activities.order_by('-date')[:50]
 
     context = {
         'managers': managers,
         'selected_manager': manager_id,
         'date_from': date_from_str,
         'date_to': date_to_str,
-        'total_companies': total_companies,
+        'total_created_companies': total_created_companies,
         'manager_created_companies': manager_created_companies,
-        'total_contacts': total_contacts,
+        'total_created_contacts': total_created_contacts,
         'manager_created_contacts': manager_created_contacts,
         'incoming_calls': incoming_calls,
         'outgoing_calls': outgoing_calls,
