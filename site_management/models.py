4408567a1712be51c1df75653654b27dcbe1a546
DmytroTymar1992 <timardmitro@gmail.com>
2025-04-03 13:04:44 +0000

    new

--- site_management/models.py
+++ site_management/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Seeker(models.Model):
     user_id = models.CharField(max_length=50, unique=True, verbose_name="ID користувача")
-    first_name = models.CharField("Ім'я", max_length=100, verbose_name="Ім'я")
-    last_name = models.CharField("Прізвище", max_length=100, null=True, blank=True, verbose_name="Прізвище")
+    first_name = models.CharField(max_length=100, verbose_name="Ім'я")
+    last_name = models.CharField(max_length=100, null=True, blank=True, verbose_name="Прізвище")
     phone = models.CharField(max_length=32, null=True, blank=True, verbose_name="Телефон")
-    email = models.EmailField("Email", max_length=254, null=True, blank=True, verbose_name="Email")
+    email = models.EmailField(max_length=254, null=True, blank=True, verbose_name="Email")
     created_at = models.DateTimeField("Створено", auto_now_add=True, verbose_name="Дата створення")
 
     def __str__(self):
